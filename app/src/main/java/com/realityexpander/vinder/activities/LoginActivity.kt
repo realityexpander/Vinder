ed08ec7d490bfa5f3275d9d6f056ea4f6f195c4a
Chris Athanas <chris@crowdskout.com>
2021-12-20 22:06:06 -0600

    Add login and signup activities

--- app/src/main/java/com/realityexpander/vinder/activities/LoginActivity.kt
+++ app/src/main/java/com/realityexpander/vinder/activities/LoginActivity.kt
@@ -1,12 +1,56 @@
 package com.realityexpander.vinder.activities
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.realityexpander.vinder.R
+import com.realityexpander.vinder.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var bind: ActivityLoginBinding
+
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private val firebaseAuthListener = FirebaseAuth.AuthStateListener {
+        val user = firebaseAuth.currentUser
+        if(user != null) {
+            //startActivity(TinderActivity.newIntent(this))
+            finish()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        bind = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        firebaseAuth.addAuthStateListener(firebaseAuthListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener)
+    }
+
+    fun onLogin(v: View) {
+        if(!bind.emailET.text.toString().isEmpty() && !bind.passwordET.text.toString().isEmpty()) {
+            firebaseAuth.signInWithEmailAndPassword(bind.emailET.text.toString(), bind.passwordET.text.toString())
+                .addOnCompleteListener { task ->
+                    if(!task.isSuccessful) {
+                        Toast.makeText(this, "Login error ${task.exception?.localizedMessage}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+
+    companion object {
+        fun newIntent(context: Context?) = Intent(context, LoginActivity::class.java)
     }
 }
--- app/src/main/java/com/realityexpander/vinder/activities/StartupActivity.kt
+++ app/src/main/java/com/realityexpander/vinder/activities/StartupActivity.kt
@@ -1,5 +1,7 @@
 package com.realityexpander.vinder.activities
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -14,69 +16,79 @@ const val TAG = "StartupActivity"
 
 class StartupActivity : AppCompatActivity() {
 
-    val flingContainer: SwipeFlingAdapterView = findViewById<SwipeFlingAdapterView>(R.id.frame);
-    val al = ArrayList<String>();
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_startup)
-
-        al.add("php");
-        al.add("c");
-        al.add("python");
-        al.add("java");
-
-        //choose your favorite adapter
-        val arrayAdapter = ArrayAdapter<String>(this, R.layout.item, R.id.helloText, al );
-
-        //set the listener and the adapter
-        flingContainer.adapter = arrayAdapter;
-        flingContainer.setFlingListener(object: SwipeFlingAdapterView.onFlingListener {
-            var i = 0
-
-            override fun removeFirstObjectInAdapter() {
-                // this is the simplest way to delete an object from the Adapter (/AdapterView)
-                Log.d("LIST", "removed object!");
-                al.removeAt(0);
-                arrayAdapter.notifyDataSetChanged();
-            }
-
-            override fun onLeftCardExit(dataObject: Any?) {
-                //Do something on the left!
-                //You also have access to the original object.
-                //If you want to use it just cast it (String) dataObject
-                Toast.makeText(this@StartupActivity, "Left!", Toast.LENGTH_SHORT).show();
-            }
-
-            override fun onRightCardExit(dataObject: Any?) {
-                Toast.makeText(this@StartupActivity, "Right!", Toast.LENGTH_SHORT).show();
-            }
-
-            override fun onAdapterAboutToEmpty(itemsInAdapter: Int) {
-                // Ask for more data here
-                al.add("XML $i")
-                arrayAdapter.notifyDataSetChanged()
-                Log.d("LIST", "notified")
-                i++
-            }
-
-            override fun onScroll(p0: Float) {
-                flingContainer[flingContainer.childCount-1].alpha=(1.0f-p0/5.0f)
-            }
-        })
-
-        // Optionally add an OnItemClickListener
-        flingContainer.setOnItemClickListener { position, dataObject ->
-            Toast.makeText(
-                this@StartupActivity,
-                "Clicked! $position $dataObject",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
     }
 
-    fun onLogin(view: View) {}
-    fun onSignup(view: View) {}
+    fun onLogin(v: View) {
+        startActivity(LoginActivity.newIntent(this))
+    }
+
+    fun onSignup(v: View) {
+//        startActivity(SignupActivity.newIntent(this))
+    }
+
+    companion object {
+        fun newIntent(context: Context?) = Intent(context, StartupActivity::class.java)
+    }
 
 
 }
+
+
+//val flingContainer: SwipeFlingAdapterView = findViewById<SwipeFlingAdapterView>(R.id.frame);
+//val al = ArrayList<String>();
+//
+//        al.add("php");
+//        al.add("c");
+//        al.add("python");
+//        al.add("java");
+//
+//        //choose your favorite adapter
+//        val arrayAdapter = ArrayAdapter<String>(this, R.layout.item, R.id.helloText, al );
+//
+//        //set the listener and the adapter
+//        flingContainer.adapter = arrayAdapter;
+//        flingContainer.setFlingListener(object: SwipeFlingAdapterView.onFlingListener {
+//            var i = 0
+//
+//            override fun removeFirstObjectInAdapter() {
+//                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+//                Log.d("LIST", "removed object!");
+//                al.removeAt(0);
+//                arrayAdapter.notifyDataSetChanged();
+//            }
+//
+//            override fun onLeftCardExit(dataObject: Any?) {
+//                //Do something on the left!
+//                //You also have access to the original object.
+//                //If you want to use it just cast it (String) dataObject
+//                Toast.makeText(this@StartupActivity, "Left!", Toast.LENGTH_SHORT).show();
+//            }
+//
+//            override fun onRightCardExit(dataObject: Any?) {
+//                Toast.makeText(this@StartupActivity, "Right!", Toast.LENGTH_SHORT).show();
+//            }
+//
+//            override fun onAdapterAboutToEmpty(itemsInAdapter: Int) {
+//                // Ask for more data here
+//                al.add("XML $i")
+//                arrayAdapter.notifyDataSetChanged()
+//                Log.d("LIST", "notified")
+//                i++
+//            }
+//
+//            override fun onScroll(p0: Float) {
+//                flingContainer[flingContainer.childCount-1].alpha=(1.0f-p0/5.0f)
+//            }
+//        })
+//
+//        // Optionally add an OnItemClickListener
+//        flingContainer.setOnItemClickListener { position, dataObject ->
+//            Toast.makeText(
+//                this@StartupActivity,
+//                "Clicked! $position $dataObject",
+//                Toast.LENGTH_SHORT
+//            ).show()
+//        }
