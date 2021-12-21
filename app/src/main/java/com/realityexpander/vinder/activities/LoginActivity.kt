f3b6332033af853b87b56372580041008cfcd7eb
Chris Athanas <chris@crowdskout.com>
2021-12-21 01:39:46 -0600

    Add Login & Signup validation

--- app/src/main/java/com/realityexpander/vinder/activities/LoginActivity.kt
+++ app/src/main/java/com/realityexpander/vinder/activities/LoginActivity.kt
@@ -3,9 +3,13 @@ package com.realityexpander.vinder.activities
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.realityexpander.vinder.R
 import com.realityexpander.vinder.databinding.ActivityLoginBinding
@@ -28,6 +32,13 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bind = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(bind.root)
+
+        // Setup DataBind Click Handlers
+        bind.handlers = this
+
+        //setup reset text-error message listeners
+        setOnTextChangedListener(bind.emailET, bind.emailTIL)
+        setOnTextChangedListener(bind.passwordET, bind.passwordTIL)
     }
 
     override fun onStart() {
@@ -41,14 +52,49 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun onLogin(v: View) {
-        if(!bind.emailET.text.toString().isEmpty() && !bind.passwordET.text.toString().isEmpty()) {
-            firebaseAuth.signInWithEmailAndPassword(bind.emailET.text.toString(), bind.passwordET.text.toString())
-                .addOnCompleteListener { task ->
-                    if(!task.isSuccessful) {
-                        Toast.makeText(this, "Login error ${task.exception?.localizedMessage}", Toast.LENGTH_SHORT).show()
-                    }
-                }
+        var proceed = true
+
+        if (bind.emailET.text.isNullOrEmpty()) {
+            bind.emailTIL.error = "Email is required"
+            bind.emailTIL.isErrorEnabled = true
+            proceed = false
         }
+        if (bind.passwordET.text.isNullOrEmpty()) {
+            bind.passwordTIL.error = "Password is required"
+            bind.emailTIL.isErrorEnabled = true
+            proceed = false
+        }
+        if (proceed) {
+
+            if (bind.emailET.text.toString().isNotEmpty()
+                && bind.passwordET.text.toString().isNotEmpty()
+            ) {
+                firebaseAuth.signInWithEmailAndPassword(
+                    bind.emailET.text.toString(),
+                    bind.passwordET.text.toString()
+                )
+                    .addOnCompleteListener { task ->
+                        if (!task.isSuccessful) {
+                            Toast.makeText(
+                                this,
+                                "Login error ${task.exception?.localizedMessage}",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+            }
+        }
+    }
+
+    // To remove the error warning when user types into the fields again
+    private fun setOnTextChangedListener(et: EditText, til: TextInputLayout) {
+        et.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                til.isErrorEnabled = false
+            }
+        })
     }
 
     override fun onBackPressed() {
--- app/src/main/java/com/realityexpander/vinder/activities/SignupActivity.kt
+++ app/src/main/java/com/realityexpander/vinder/activities/SignupActivity.kt
@@ -4,9 +4,13 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.view.Window
+import android.widget.EditText
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.realityexpander.vinder.R
@@ -32,6 +36,12 @@ class SignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bind = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(bind.root)
+
+        bind.handlers = this
+
+        //setup reset text-error message listeners
+        setOnTextChangedListener(bind.emailET, bind.emailTIL)
+        setOnTextChangedListener(bind.passwordET, bind.passwordTIL)
     }
 
     override fun onStart() {
@@ -45,29 +55,70 @@ class SignupActivity : AppCompatActivity() {
     }
 
     fun onSignup(v: View) {
-        if (bind.emailET.text.toString().isNotEmpty() && bind.passwordET.text.toString()
-                .isNotEmpty()
-        ) {
-            // Create user
-            firebaseAuth.createUserWithEmailAndPassword(
-                bind.emailET.text.toString(),
-                bind.passwordET.text.toString()
-            )
-                .addOnCompleteListener { task ->
-                    if (!task.isSuccessful) {
-                        Toast.makeText(this, "Signup error ${task.exception?.localizedMessage}",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    } else {
-                        val email = bind.emailET.text.toString()
-                        val userId = firebaseAuth.currentUser?.uid ?: ""
+        var proceed = true
 
-                        // Save User to DB
-                        val user = User(userId, "", "", email, "", "", "")
-                        firebaseDatabase.child(DATA_USERS_COLLECTION).child(userId).setValue(user)
-                    }
-                }
+        if (bind.emailET.text.isNullOrEmpty()) {
+            bind.emailTIL.error = "Email is required"
+            bind.emailTIL.isErrorEnabled = true
+            proceed = false
         }
+        if (bind.passwordET.text.isNullOrEmpty()) {
+            bind.passwordTIL.error = "Password is required"
+            bind.emailTIL.isErrorEnabled = true
+            proceed = false
+        }
+        if (proceed) {
+            if (bind.emailET.text.toString().isNotEmpty()
+                && bind.passwordET.text.toString().isNotEmpty()
+            ) {
+                // 1. Create user in Firebase Authentication
+                firebaseAuth.createUserWithEmailAndPassword(
+                    bind.emailET.text.toString(),
+                    bind.passwordET.text.toString()
+                )
+                    .addOnCompleteListener { task ->
+                        if (!task.isSuccessful) {
+                            Toast.makeText(
+                                this, "Signup error ${task.exception?.localizedMessage}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            val email = bind.emailET.text.toString()
+                            val userId = firebaseAuth.currentUser?.uid ?: ""
+
+                            // 2. Save User to DB
+                            var user: User? = User(userId, "", "", email, "", "", "")
+                            firebaseDatabase.child(DATA_USERS_COLLECTION)
+                                .child(userId)
+                                .setValue(user)
+                                .addOnSuccessListener {
+                                    Toast.makeText(
+                                        this, "User saved!",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                                .addOnFailureListener {
+                                    Toast.makeText(
+                                        this, "Signup error ${task.exception?.localizedMessage}",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    user = null
+                                }
+                        }
+                    }
+            }
+        }
+    }
+
+    // To remove the error warning when user types into the fields again
+    private fun setOnTextChangedListener(et: EditText, til: TextInputLayout) {
+        et.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                til.isErrorEnabled = false
+            }
+        })
     }
 
     override fun onBackPressed() {
